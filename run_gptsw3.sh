25ce5203802f604703e1c25645344bcf7f707726
Dan Bergh Johnsson <dan.bergh.johnsson@omegapoint.se>
2023-12-07 00:33:11 +0100

    Python model code for running GPT-SW3 in various version and various ways

---
+++ run_gptsw3.sh
@@ -0,0 +1,15 @@
+echo "Starting run_gptsw3.sh"
+date -u
+touch tmp.txt
+# cat hf_token.source
+# source hf_token.source
+source env.source
+echo "Inside run_gptsw3.sh ${HF_TOKEN}"
+export HF_TOKEN
+rm stdout_dump.txt
+rm nohup.out
+nohup python gptsw3.py ${HF_TOKEN} 2>&1 &
+echo "Started model run, exiting run_gptsw3.sh"
+echo "Tailing nohup.out in background"
+sleep 1
+tail -f nohup.out &
